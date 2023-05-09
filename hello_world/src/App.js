9a1b32a38b26f353b685b6e30dc47d3918bdccad
BigChaney <johnchaneyim@gmail.com>
2023-05-09 16:55:24 -0400

    Main commit

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,7 +1,19 @@
-function App() {
-  return (
-      <h1>Hello World</h1>
-  );
+import React from "react";
+import './App.css';
+
+const App = () => {
+    const [todos, setTodos] = React.useState([]);
+    const [todos, setTodos] = React.useState("");
+
+    return (
+        <div className = "App">
+            <h1>Todo List</h1>
+            <form>
+                <input type = "text" align = "right" />
+                <button type = "submit">Add Todo</button>
+            </form>
+        </div>
+    )
 }
 
 export default App;
